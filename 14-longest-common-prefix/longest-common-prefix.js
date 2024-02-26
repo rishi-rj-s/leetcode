38bd34266c81f15ba4758fc9a410325593aba11f
Rishiraj S <131572506+rishi-rj-s@users.noreply.github.com>
2024-02-26 18:12:56 +0530

    Time: 70 ms (14.17%) | Memory: 49.3 MB (36.74%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 0) {
+        return "";
+    }
+
+    let prefix = strs[0];
+
+    for (let word of strs) {
+        while (word.indexOf(prefix) !== 0) {
+            prefix = prefix.slice(0, -1);
+            if (prefix === '') return ''
+        }
+    }
+    return prefix;
+};
