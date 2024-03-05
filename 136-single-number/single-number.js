2a883499bc092b60057ce098d0cfaecf629f0dd8
Rishiraj S <131572506+rishi-rj-s@users.noreply.github.com>
2024-03-05 15:44:51 +0530

    Time: 50 ms (91.88%) | Memory: 50.2 MB (57.24%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -3,17 +3,7 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    let result;
-    nums.forEach(num => {
-        let count = 0;
-        nums.forEach(innerNum => {
-            if (num === innerNum) {
-                count++;
-            }
-        });
-        if (count === 1) {
-            result = num;
-        }
-    });
-    return result;
+    return nums.reduce((acc, cur)=>{
+        return acc^cur;
+    })
 };
