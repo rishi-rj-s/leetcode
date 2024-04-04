c03a5f3f43a60300c25c97052f72197716950451
Rishiraj S <131572506+rishi-rj-s@users.noreply.github.com>
2024-04-04 14:03:37 +0530

    Time: 54 ms (50.43%) | Memory: 48.8 MB (62.32%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    let count = 0;
+    let maxNum = 0;
+    for (let c of s) {
+        if (c === '(') {
+            count++;
+            if (maxNum < count)
+                maxNum = count;
+        } else if (c === ')') {
+            count--;
+        }
+    }
+    return maxNum;
+};
