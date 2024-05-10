c40c9d3278ba59845d9cecac7bb25f03239f16f6
Rishiraj S <131572506+rishi-rj-s@users.noreply.github.com>
2024-05-10 16:00:21 +0530

    Time: 69 ms (82.35%) | Memory: 50.9 MB (88.24%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.js
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.js
@@ -4,13 +4,17 @@
  * @return {number[]}
  */
 var kthSmallestPrimeFraction = function(arr, k) {
-    let fractions = [], ans = []
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            fractions.push([arr[i] / arr[j], arr[i], arr[j]])
+    let l = 0, r = 1;
+    while (true) {
+        const m = (l + r) / 2;
+        let count = 0, a = 0, b = 1;
+        for (let i = 0, j = 1; i < arr.length - 1; i++) {
+            while (j < arr.length && arr[i] / arr[j] > m) j++;
+            count += arr.length - j;
+            if (arr[i] / arr[j] > a / b) [a, b] = [arr[i], arr[j]];
         }
+        if (count < k) l = m;
+        else if (count > k) r = m;
+        else return [a, b];
     }
-    fractions.sort((a, b) => a[0] - b[0]);
-
-    return [fractions[k - 1][1], fractions[k - 1][2]]
 };
