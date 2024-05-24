86d531d756f96931ce97623e183fde12d770fc5b
Rishiraj S <131572506+rishi-rj-s@users.noreply.github.com>
2024-05-24 17:00:36 +0530

    Time: 55 ms (84.62%) | Memory: 51.5 MB (76.92%) - LeetSync

---
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} words
+ * @param {character[]} letters
+ * @param {number[]} score
+ * @return {number}
+ */
+var maxScoreWords = function(words, letters, score) {
+    let lettersSize = new Array(26).fill(0);
+    for (let char of letters) {
+        lettersSize[char.charCodeAt() - 'a'.charCodeAt()]++;
+    }
+
+    let maxScore = 0;
+    const backTrack = function (start, curSize, curScore) {
+        for (let i = start; i < words.length; i++) {
+            let word = words[i];
+            let remainSize = [...curSize];
+            let nextScore = curScore;
+            let j = 0;
+            while (j < word.length) {
+                let idx = word[j].charCodeAt() - 'a'.charCodeAt();
+                if (remainSize[idx]-- === 0) break;
+                nextScore += score[idx];
+                j++;
+            }
+            if (j === word.length) {
+                maxScore = Math.max(maxScore, nextScore);
+                backTrack(i + 1, remainSize, nextScore);
+            }
+        }
+    }
+
+    backTrack(0, lettersSize, 0);
+    return maxScore;
+};
